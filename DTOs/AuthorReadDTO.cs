161c80774b6f5f954e6c0650938a603d77162ca8
Hallgató László <hallgato.laszlo1124@gmail.com>
2026-01-05 20:41:52 +0100

    2.3 A GET műveletekhez szükséges AuthorReadDTO (könyvek listájával) és BookReadDTO (szerző nevével) modellek definiálása.

---
+++ DTOs/AuthorReadDTO.cs
@@ -0,0 +1,11 @@
+﻿using KonyvtarNyilvantartas.Models;
+
+namespace KonyvtarNyilvantartas.DTOs
+{
+    public class AuthorReadDTO
+    {
+        public string Name { get; set; } = null!;
+        public DateTime? BirthDate { get; set; }
+        public ICollection<Book>? Books { get; set; };
+    }
+}
---
+++ DTOs/BookReadDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace KonyvtarNyilvantartas.DTOs
+{
+    public class BookReadDTO
+    {
+        public string Title { get; set; } = null!;
+        public int PublicationYear { get; set; }
+        public string AuthorName { get; set; } = null!;
+    }
+}
