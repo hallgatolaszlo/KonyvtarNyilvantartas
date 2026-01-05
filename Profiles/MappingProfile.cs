a39248b56eee35d141739684aef307ca6871a67b
Hallgató László <hallgato.laszlo1124@gmail.com>
2026-01-05 20:55:44 +0100

    2.4 AutoMapper (vagy kézi mapping) beállítása az Entitás <-> DTO konverzióhoz.

---
+++ Profiles/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using KonyvtarNyilvantartas.DTOs;
+using KonyvtarNyilvantartas.Models;
+
+namespace KonyvtarNyilvantartas.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookReadDTO>()
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => src.Author.Name));
+            CreateMap<BookCreateDTO, Book>();
+
+
+            CreateMap<Author, AuthorReadDTO>();
+            CreateMap<AuthorCreateDTO, Author>();
+        }
+    }
+}
