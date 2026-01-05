43ce05917d5ebb916d32d2d56bb464722effb303
csudi <hallgato.laszlo1124@gmail.com>
2026-01-05 13:42:47 +0100

    1.4 LibraryDbContext létrehozása és konfigurálása az SQLite providerrel. 2.1 Migráció létrehozása és az adatbázis frissítése.

---
+++ Data/LibraryDbContext.cs
@@ -0,0 +1,11 @@
+﻿using KonyvtarNyilvantartas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KonyvtarNyilvantartas.Data
+{
+    public class LibraryDbContext(DbContextOptions<LibraryDbContext> options) : DbContext(options)
+    {
+        public DbSet<Book> Books { get; set; } = null!;
+        public DbSet<Author> Authors { get; set; } = null!;
+    }
+}
